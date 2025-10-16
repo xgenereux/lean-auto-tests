a9a341095cd4ed253c20652d10a18a8abd096829
Xavier Genereux <xaviergenereux@hotmail.com>
2025-10-16 17:49:49 +0200

    update aesop with all changes

--- lakefile.lean
+++ lakefile.lean
@@ -11,4 +11,4 @@ lean_lib «Auto» {
   -- add any library configuration options here
 }
 
-require aesop from git "https://github.com/leanprover-community/aesop" @ "forward-eval-hyps"
+require aesop from git "https://github.com/leanprover-community/aesop" @ "forward-eval-all"
