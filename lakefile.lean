f7940d6b469386db3cf71182d87f61e9667e73f3
Jannis Limperg <limperg@amazon.com>
2025-12-04 00:58:39 +0100

    Update Aesop
    This version has a lazy forward state.

--- lakefile.lean
+++ lakefile.lean
@@ -11,4 +11,4 @@ lean_lib «Auto» {
   -- add any library configuration options here
 }
 
-require aesop from git "https://github.com/leanprover-community/aesop" @ "forward-eval-redesign"
+require aesop from git "https://github.com/leanprover-community/aesop" @ "forward-eval-redesign-lazy"
